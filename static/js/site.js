230a7d403217abdb133aa374811072c35c78db82
tectonick <korn9509@gmail.com>
2022-08-29 09:31:37 +0400

    Scroll top and progress bar

--- static/js/site.js
+++ static/js/site.js
@@ -1,11 +1,42 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        // If the element is visible
-        if (entry.isIntersecting) {
-          // Add the animation class
-          entry.target.classList.add('in-animation');
-        }
-      });
+// In animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add("in-animation");
+    }
+  });
 });
-  
-document.querySelectorAll('.in-animate').forEach(el=>observer.observe(el));
+
+document.querySelectorAll(".in-animate").forEach((el) => observer.observe(el));
+
+//Scroll to top button
+const showOnPx = 200;
+const backToTopButton = document.querySelector(".back-to-top");
+const pageProgressBar = document.querySelector(".top-progress-bar");
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  const scrolledPercentage =
+    (scrollContainer().scrollTop /
+      (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+    100;
+
+  pageProgressBar.style.width = `${scrolledPercentage}%`;
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+const goToTop = () => {
+  document.body.scrollIntoView();
+};
+
+backToTopButton.addEventListener("click", goToTop);
