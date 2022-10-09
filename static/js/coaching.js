58c073c64a7ef721a82f1c46e26c75a6dad337c2
tectonick <korn9509@gmail.com>
2022-10-09 22:45:46 +0400

    videos new style, links, some image positioning

--- static/js/coaching.js
+++ static/js/coaching.js
@@ -1,22 +1,27 @@
-var iframe = document.querySelector('iframe');
-var player = new Vimeo.Player(iframe);
+
+var options = {
+    url: "https://player.vimeo.com/video/749910680?h=da0cb51709",
+    responsive: true,
+    controls: false,
+    title: false,
+    portrait: false
+  };
+
+var playerdiv = document.getElementById('main-vimeo');
+var player = new Vimeo.Player(playerdiv, options);
 var playimage = document.getElementById("main-play-image");
 var titletext = document.getElementById("title-text");
+var mainplaydiv = document.getElementById('main-play');
+var playMainVimeoBtn = document.getElementById('play-main-vimeo-btn');
 
-iframe.addEventListener("click", () => {
-    playimage.style.display = 'none'
+playMainVimeoBtn.addEventListener("click", () => {
+    mainplaydiv.classList.add('transparent');
+    player.setCurrentTime(103);
     player.play();
 })
 
 player.on('timeupdate', data=>{
-    if (data.percent>=0.99){
-        console.log("hide");
-        playimage.style.opacity = 1;    
-        titletext.style.opacity = 1;  
-        iframe.style.opacity = 0;
+    if (data.percent>=0.985){
+        mainplaydiv.classList.remove('transparent');
     }
 })
-
-player.on("ended", (data) => {
-    iframe.style.display = 'none';
-});
