9fb79145bf12de98293c55b2995d6cd9fdb537bc
tectonick <korn9509@gmail.com>
2022-11-20 01:38:00 +0400

    canonical

--- routes/main.js
+++ routes/main.js
@@ -6,6 +6,7 @@ router.get("/", async (_req, res) => {
     title: "TrainedByNik - Build A Head-Turning Physique And Live A More Powerful Life",
     description:
       "Meet coach Nik | Online fitness coaching | Client success stories | YouTube | Contact",
+      canonical: "https://trainedbynik.fit",
   });
 });
 
@@ -14,6 +15,7 @@ router.get("/coaching", async (_req, res) => {
     title: `TrainedByNik's Online Fitness Coaching Program`,
     description:
       "Build A Head-Turning Physique, Become A High-Performance Machine, And Start Living A More Powerful Life",
+      canonical: "https://trainedbynik.fit/coaching",
   });
 });
 
